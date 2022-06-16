39c8aa92d9a9641b87c5f0b1c74d86f69dd76311
efrain <efsolora@gmail.com>
2022-06-16 12:44:07 -0500

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const Contact = lazy(() => import("./components/Contact"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<h2>Te toca esperar...</h2>}>
+        <Router>
+          <Link to="/">
+            <button>Home</button>{" "}
+          </Link>
+          <Link to="/About">
+            <button>About</button>{" "}
+          </Link>
+          <Link to="/Contact">
+            <button>Contact</button>{" "}
+          </Link>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/About" element={<About />}></Route>
+            <Route path="/Contact" element={<Contact />}></Route>
+          </Routes>
+        </Router>
+      </Suspense>
     </div>
   );
 }
